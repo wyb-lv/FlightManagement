3c4c5d34f5e09c4d4e2431ce838b7e8cff791255
ZhugeB1ao <doanhuynhgiabao0508@gmail.com>
2024-12-18 11:01:25 +0700

    bao commit 11:1 18/12

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         EmployeeManagement management = new EmployeeManagement();
+        runEmployeeManagement(management);
+    }
+
+    public static void runEmployeeManagement(EmployeeManagement management) {
         management.run();
     }
 }
